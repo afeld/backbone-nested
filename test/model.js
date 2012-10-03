4ef435b5636e5513b123d12ef1376bb1d72c1c34
Isak Bakken <isak@klarna.com>
2012-10-03 22:11:43 +0200

    Add Enviroment to model test

--- test/model.js
+++ test/model.js
@@ -5,6 +5,45 @@ $(document).ready(function() {
   var oldModel = Backbone.Model;
   Backbone.Model = Backbone.NestedModel;
 
+
+  var Environment = this.Environment = function(){};
+
+  _.extend(Environment.prototype, {
+
+    ajax: Backbone.ajax,
+
+    sync: Backbone.sync,
+
+    setup: function() {
+      var env = this;
+
+      // Capture ajax settings for comparison.
+      Backbone.ajax = function(settings) {
+        env.ajaxSettings = settings;
+      };
+
+      // Capture the arguments to Backbone.sync for comparison.
+      Backbone.sync = function(method, model, options) {
+        env.syncArgs = {
+          method: method,
+          model: model,
+          options: options
+        };
+        env.sync.apply(this, arguments);
+      };
+    },
+
+    teardown: function() {
+      this.syncArgs = null;
+      this.ajaxSettings = null;
+      Backbone.sync = this.sync;
+      Backbone.ajax = this.ajax;
+    }
+
+  });
+
+
+
   var proxy = Backbone.Model.extend();
   var klass = Backbone.Collection.extend({
     url : function() { return '/collection'; }
