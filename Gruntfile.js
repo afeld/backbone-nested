72a4a6a5e39f09a2febec07cf8d582f87e04e3f1
Gary Katsevman <git@gkatsev.com>
2013-10-23 13:32:41 -0400

    use backbone tests directly

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
       },
     },
     qunit: {
-      index: ['test/index.html']
+      index: ['test/index.html', 'test/backbone.html']
     },
     watch: {
       files: ['<config:lint.files>', 'test/**'],
